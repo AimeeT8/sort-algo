b52ce47b514c62e6636444608477b0a79094216a
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-10-30 10:04:46 -0400

    Initial commit.

---
+++ SortAlgos/SortAlgosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortAlgosApp.swift
+//  SortAlgos
+//
+//  Created by Aimee Temple on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct SortAlgosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
