e5234e95278607448ba0e39a8038af04f8b4ef57
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-10-30 10:23:34 -0400

    Add the state property in content view to map over the values 1 to 100. Add the Geometry Reader to display the width and height size based on the number.

--- SortAlgos/ContentView.swift
+++ SortAlgos/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var values = (1...100).map(SortValue.init).shuffled()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Sort-Algos")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            GeometryReader { proxy in
+                HStack(spacing: 0) {
+                    ForEach(values) { value in
+                            Rectangle()
+                            .fill(value.color)
+                            .frame(width: proxy.size.width * 0.01, height: proxy.size.height * Double(value.id) / 100)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
